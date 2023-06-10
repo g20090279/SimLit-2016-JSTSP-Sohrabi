d61b401a1b7dd9c33338a8387f3711a4a32ecf2f
Zekai Liang <liang@LIANG-WORK.localdomain>
2023-06-10 23:42:43 +0200

    add algorithm from Reference 33

--- README.md
+++ README.md
@@ -3,11 +3,31 @@
 Reproduce the following paper in Matlab:
 
 - Sohrabi, F. & Yu, W. (2016). Hybrid digital and analog beamforming design
-  for large=scale antenna arrays. IEEE Journal of selected topics in signal
-  processing, 10(3).
+  for large-scale antenna arrays. IEEE Journal of selected topics in signal
+  processing, 10(3). https://doi.org/10.1109/JSTSP.2016.2520912
+
+## Brief Conclusion of This Paper
+
+
 
 # Reproduced Results
+
+## Introduction of the Matlab Scripts
+
+Run "main_su.m" for figure 2 (single-user case), and "main_mu.m" for figure 5 (multi-user case). It is recommended to save the generated channels in your local storage by modifying the code. Afterwards, load the generated channels for simulation.
+
+## Single-user Case
 
 ![figure2](./images/figure2.jpg)
 
 *Fig. 2. Spectral efficiencies achieved by different methods in a $64x16$ MIMO system where $N^{RF}=N_s=6$. For hybrid beamforming methods, the use of infinite resolution phase shifters is assumed.*
+
+## Multi-user Case
+
+![figure 5](./images/figure5.jpg)
+
+*Fig. 5. Sum rate achieved by different methods in an 8-user MISO system with N=64. For hybrid beamforming methods, the use of infinite resolution phase shifters is assumed.*
+
+
+
+
--- main_mu.m
+++ main_mu.m
@@ -1,5 +1,5 @@
 % Multiuser
-% Last revised: May 25, 2023
+% Last revised: June 09, 2023
 % Zekai Liang, liang@ice.rwth-aachen.de
 
 %% Settings
@@ -16,16 +16,22 @@ numRfTx2 = numUser;
 numPath = 15;
 
 %% Generate or Load Channels
-numMC = 100;  % number of iterations for Monte-Carlo simulation
+numMC = 10;  % number of iterations for Monte-Carlo simulation
 % generate_and_save_channel(numAntTx,numAntRx,numPath,numMC);\
 
 %% Initialize Variables for Monte-Carlo Simulation
 capFdSum = 0;
 capPropSum = 0;
+capRef33Sum = 0;
 
 for iMC = 1:numMC
 
     %% Load or Generate Channels
+
+    % NOTE: the dimension of generated channel of each user is a row
+    % vector. Therefore, the combined channel matrix is H=[h1',...,hK']',
+    % where (.)' is the conjugate transpose (Hermitian).
+
     % Generate Multiuser Channel Instance
 %     generate_and_save_channel_mu(numUser,numAntTx,numAntRx,numPath,numMC,'combined');
 
@@ -35,10 +41,12 @@ for iMC = 1:numMC
     %% Fully-Digital Precoder
     Vd = chn'*(chn*chn')^(-1);
     
-    % For Each SNR
+    % For Each SNR (Assume the Noise Power equals 1)
     capFd = zeros(numUser,snrLen);
     for iSnr = 1:snrLen
-        % Use Water-Filling to Allocate Power
+
+        % Use Water-Filling to Allocate Power, to Meet the Power Constraint
+        % (The total beamformer power constraint is ||Vd||_F^2=P)
         powFd = water_filling_MIMO_ZF(snrVec(iSnr),ones(numUser,1),Vd);
         VdNew = Vd*diag(sqrt(powFd));
     
@@ -57,9 +65,8 @@ for iMC = 1:numMC
 
 
     %% Proposed Algorithm
-    % For Each SNR
+    % For Each SNR (Assume the Noise Power equals 1)
     capMu = zeros(numUser,snrLen);
-    
     for iSnr = 1:snrLen
 
         % Caculate Analog Precoder by Algorithm 3
@@ -82,20 +89,54 @@ for iMC = 1:numMC
     % the Sum Capacity of All Users
     capPropSum = capPropSum + sum(capMu)';
 
-    %
-    disp([num2str(iMC),'-th iteration finished!'])
+    
+    %% Reference [33]
+    % The Phase of Analog Beamformer Is the Phase of Conjugate Transpose of
+    % the Composite Channel Matrix
+    Vrf = 1/numAntTx * exp(1i*angle(chn'));
+    
+    % the Equivalent Channel
+    chnEqv = chn*Vrf;
+
+    % ZF Digital Beamformer
+    Vd = chnEqv'*(chnEqv*chnEqv')^-1;
+
+    % For Each SNR (Assume the Noise Power equals 1)
+    capRef33 = zeros(numUser,snrLen);
+    for iSnr = 1:snrLen
+        % Use Water-Filling Algorithm to Meet Power Constraint
+        % (The total beamformer power constraint is ||Vrf*Vd||_F^2=P)
+        powRef33 = water_filling_MIMO_ZF(snrVec(iSnr),ones(numUser,1),Vrf*Vd);
+        VdNew = Vd*diag(sqrt(powRef33));
+
+        % Calculate Multi-User Capacity, i.e. Sum-Rate of All Users
+        for iUser = 1:numUser
+            chnUser = chn(iUser,:);
+            gainTmp = abs(chnUser*Vrf*VdNew).^2;
+            gainTmpBar = gainTmp;
+            gainTmpBar(iUser) = [];
+            capRef33(iUser,iSnr) = log2( 1 + gainTmp(iUser) / (1 + sum(gainTmpBar)) );
+        end
+    end
+
+    % the Sum Capacity of All Users
+    capRef33Sum = capRef33Sum + sum(capRef33)';
+
+    %% Print the Progress
+    disp([num2str(iMC),'-th Monte-Carlo iteration finished!'])
+
 end
 
 %% Averge Data After Monte-Carlo
 capFdSum = capFdSum/numMC;
 capPropSum = capPropSum/numMC;
+capRef33Sum = capRef33Sum/numMC;
 
 %% Plot Figure
 figure;
-plot(snrVecDb,capFdSum,'--k',snrVecDb,capPropSum,'-ob');
+plot(snrVecDb,capFdSum,'--k',snrVecDb,capPropSum,'-ob',snrVecDb,capRef33Sum,'-dr');
 hold on;
 grid on;
 xlabel('SNR (dB)');
 ylabel('Sum Rate (bits/s/Hz)');
-legend("Fully-Digital ZF","Proposed Algorithm for N^{RF}=9");
-title('Figure 5')
+legend("Fully-Digital ZF","Proposed Algorithm for N^{RF}=9","Hybrid beamforming in [33], N^{RF}=8");
